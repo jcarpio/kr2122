05507380802c1d98f422c9d41eb00edb8a9563c2
jcarpio <jose.carpio@gmail.com>
2022-03-01 12:31:34 +0100

    Create fibonacci.pl

---
+++ fibonacci.pl
@@ -0,0 +1,20 @@
+/*
+
+1. fibonacci(0) = 0
+
+2. fibonacci(1) = 1
+
+3. n > 1
+fibonacci(n) = fibonacci(n-1) +
+               fibonacci(n-2)
+			   
+			   
+fibonacci(N, R).
+it is true if R unify with the value
+of fibonacci(N)
+			   
+			   */
+			   
+% fibonacci(0) = 0			   
+fibonacci(0, 0).
+
