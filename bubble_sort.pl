f9c6cf58fce40981c48778e726d56f8fa58cd1ca
jcarpio <jose.carpio@gmail.com>
2022-04-19 12:35:24 +0200

    Update bubble_sort.pl

--- bubble_sort.pl
+++ bubble_sort.pl
@@ -6,12 +6,16 @@
 
 bubble_sort(L, L):- sorted(L).
 
-
-bubble_sort(L,  ):- append(L1, [E1,E2|L2], L),
+bubble_sort(L, R2):- 
+  append(L1, [E1,E2|L2], L),
   E1 > E2,
-  append(
-
-
+  append(L1, [E2,E1|L2], R),
+  bubble_sort(R, R2).
+  
+bubble_sort(L, R):- 
+  append(_, [E1,E2|_], L),
+  E1 =< E2,  
+  bubble_sort(L, R).
 
 
 /*
@@ -22,9 +26,10 @@ sorted(+List)
 */
 
 sorted([]).
-sorted([_]).
-sorted([H1,H2|Tail]) :- H1 =< H2, sorted([H2|Tail]).
 
+sorted([_]).
+
+sorted([E1,E2|Tail]) :- E1 =< E2, sorted([E2|Tail]).
 
 
 /*
@@ -39,3 +44,28 @@ sorted([H1,H2|Tail]) :- H1 =< H2, sorted([H2|Tail]).
   
 
 */
+
+
+
+/* 
+
+ generate_list(+N, -List)
+   it is true if List unify with a list
+   of N radom generated numbers between
+   1 and N.
+   
+*/
+
+generate_list(0, []).
+
+
+generate_list(N, [Head|R]):-
+    N > 0,
+    N2 is N-1,
+	generate_list(N2, R),
+	Head is random(N).
+
+
+
+
+
